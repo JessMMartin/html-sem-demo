59187f5588c0480cc46030f8193d59637f85fb89
Jess <jessicammartin@live.co.uk>
2023-06-08 18:29:46 +0100

    some added javascript features

---
+++ zdemo.js
@@ -0,0 +1,11 @@
+
+
+
+console.log ("I'm on this course to learn how to make the best pasta website in the North West");
+
+let userName = prompt("Hi User :) What is your favorite pasta?");
+console.log(userName)
+
+const welcomeMsg =alert("Welcome Pasta Lover") + userName
+
+document.write
